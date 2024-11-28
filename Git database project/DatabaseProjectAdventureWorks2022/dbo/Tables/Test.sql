241020cbead494d709babfa0c44b9fe40925fafa
Aleksandar Beshkov <alexander.beshkov@gmail.com>
2024-11-28 18:57:54 +0200

    bringing changes from db to project

--- Git database project/DatabaseProjectAdventureWorks2022/dbo/Tables/Test.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[Test]
-(
-    [TestTableID]   INTEGER         IDENTITY(1,1) NOT NULL,
-    [TestTableNote] VARCHAR(150)    NULL,
-    [NoteDate]      SMALLDATETIME   CONSTRAINT DF_TestTableNoteDate DEFAULT(getdate()) NOT NULL,
-    CONSTRAINT PK_TestTableID PRIMARY KEY CLUSTERED ([TestTableID])
-);
